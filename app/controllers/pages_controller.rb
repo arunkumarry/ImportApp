29badeaa84e1b7dd676f9a116be85373a4be82b9
arun kumar <arunkumar.ry1@gmail.com>
2019-05-16 20:11:47 +0530

    added csv file in public and modified code to populate db as we run it

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,28 @@ class PagesController < ApplicationController
 	require 'csv'
 
 	def home
+		CSV.foreach('public/Import task - Candidates.csv', headers: true) do |row|
+			candidate_name = row[0]
+			candidate_email = row[1]
+			candidate_phone = row[2]
+			candidate_created = DateTime.parse(row[4])
+			job = Job.find_by(name: row[3].downcase.strip)
+			candidate = Candidate.find_by(email: candidate_email)
+			if job.nil?
+				job = Job.create(name: row[3].downcase.strip)
+			end
+			if candidate.nil?
+				Candidate.create(name: candidate_name, email: candidate_email, phone: candidate_phone, job: job, created_at: candidate_created)
+			end
+		end
+
+		CSV.foreach('public/Import task - Notes.csv', headers: true) do |row|
+			candidate = Candidate.find_by(email: row[0])
+			if candidate.present?
+				notes = row[1]
+				note = Note.create(content: notes, candidate: candidate)
+			end
+		end
 	end
 
 	def import
